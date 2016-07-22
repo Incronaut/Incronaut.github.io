b15b519b49465712e3a667ba3311f4cdca35305b
Christopher Chu <cchu@finisinc.com>
2016-07-21 17:10:33 -0700

    grunt 3

--- build/tasks/shell.js
+++ build/tasks/shell.js
@@ -9,13 +9,13 @@ module.exports = function(grunt) {
             }
         },
         push_prod_s3: {
-            command: 'aws --profile rubberduck s3 cp _site/ s3://rubberduckcast.com --recursive --include \'*\' --acl \'public-read\''
+            command: 'aws --profile incronaut s3 cp _site/ s3://incronaut.com --recursive --include \'*\' --acl \'public-read\''
         },
         push_episodes: {
-            command: 'aws --profile rubberduck s3 sync _audio/ s3://episodes.rubberduckcast.com --include \'*\' --acl \'public-read\''
+            command: 'aws --profile incronaut s3 sync _audio/ s3://episodes.incronaut.com --include \'*\' --acl \'public-read\''
         },
         download_report: {
-            command: 'aws --profile rubberduck s3 sync s3://logs-rubberducking ./_s3_logs && ' +
+            command: 'aws --profile incronaut s3 sync s3://logs-incronaut ./_s3_logs && ' +
               'bundle exec request-log-analyzer -f amazon_s3 --output html --file report.html _s3_logs'
         }
     });
