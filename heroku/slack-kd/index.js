53e7c25d1504724a5ec3f5dc1a10d3f68f6aaf80
Christopher Chu <cchu@finisinc.com>
2017-05-19 16:31:46 -0700

    working on bot!

--- heroku/slack-kd/index.js
+++ heroku/slack-kd/index.js
@@ -2,30 +2,28 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-app.set('port', (process.env.PORT || 1337));
+var port = process.env.PORT || 1337;
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 	res.status(200).send('Hello World!'); 
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(port, function() {
+  console.log('Node app is running on port' + port);
 });
 
-app.post('/hello', function(req, res,next){
+app.post('/kitchenduty', function(req, res, next){
 	var username = req.body.user_name;
 	var botPayload = {
-		text: 'Hello ' + username + ', welcome to the Slack channel! Have fun:)'
+		text: "Kitchen Duty Assignment: \nMonday AM - " + username + "\nMonday PM - " + username + "\nTuesday AM - " + username + "\nTuesday PM - " + username + "\nWednesday AM - " + username + "\nWednesday PM - " + username + "\nThursday AM - " + username + "\nThursday PM - " + username + "\nFriday AM - " + username + "\nFriday PM - " + username
 	};
+
 
 	if(username !== 'slackbot'){
 		return res.status(200).json(botPayload);
 	} else {
 		return res.status(200).end();
 	}
-});
+});"
